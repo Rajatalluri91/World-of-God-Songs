ca99a20eda292eb3de3bcc64ec2300aca00fa354
Rajatalluri91 <talluriraja91@gmail.com>
2026-01-20 08:52:42 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,17 @@
-const CACHE_NAME = "world-of-god-v1";
+const CACHE_NAME = "world-of-god-v2";
 
 const ASSETS = [
-  "/World-of-God-Songs/",
-  "/World-of-God-Songs/Index.html",
-  "/World-of-God-Songs/Main Cover.jpg",
+  "/World-of-God-Songs/index.html",
+  "/World-of-God-Songs/manifest.json",
+  "/World-of-God-Songs/Main-Cover.jpg",
 
   "/World-of-God-Songs/Akashamtho.mp3",
-  "/World-of-God-Songs/Devuni Sneham.mp3",
-  "/World-of-God-Songs/New Year Song.mp3",
-  "/World-of-God-Songs/New Year Song Remix.mp3",
-  "/World-of-God-Songs/New Year Track.mp3",
+  "/World-of-God-Songs/Devuni-Sneham.mp3",
+  "/World-of-God-Songs/New-Year-Song.mp3",
+  "/World-of-God-Songs/New-Year-Song-Remix.mp3",
+  "/World-of-God-Songs/New-Year-Track.mp3",
   "/World-of-God-Songs/Panduga.mp3",
-  "/World-of-God-Songs/Raja's Christmas Song Track.mp3",
+  "/World-of-God-Songs/Rajas-Christmas-Song-Track.mp3",
   "/World-of-God-Songs/Srustilo.mp3",
   "/World-of-God-Songs/Telusa.mp3",
   "/World-of-God-Songs/Thudivaraku.mp3"
